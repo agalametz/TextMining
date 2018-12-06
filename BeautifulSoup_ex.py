da61fcd2ac853c1f7f9f65511ce43ceb25128aff
Audrey Galametz <37661757+agalametz@users.noreply.github.com>
2018-12-06 13:48:53 +0100

    Create BeautifulSoup_ex.py

---
+++ BeautifulSoup_ex.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'http://www.audreygalametz.com/'
+r = requests.get(url)
+html_doc = r.text
+soup = BeautifulSoup(html_doc)
+print(soup.title)
+print(soup.text)
